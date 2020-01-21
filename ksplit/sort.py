d747f1f52fbfae1ba80f173db4a5f5bca658e822
Luis Pedro Coelho <luis@luispedro.org>
2020-01-21 13:15:36 +1100

    ENH Add merging of streams

--- ksplit/sort.py
+++ ksplit/sort.py
@@ -1,6 +1,33 @@
 from os import path, makedirs
 import numpy as np
 
+class file_buffer:
+    '''file object wrapper with cheap peek()ing'''
+    def __init__(self, fname):
+        self.handle = open(fname, 'rb')
+        self.buf = b''
+
+
+    def close(self):
+        self.handle.close()
+
+
+    def __del__(self):
+        self.close()
+
+
+    def peek(self, nbytes):
+        if nbytes > len(self.buf):
+            nbytes -= len(self.buf)
+            read_size = max(4096, nbytes)
+            self.buf += self.handle.read(read_size)
+        return self.buf[:nbytes]
+
+
+    def consume(self, nbytes):
+        self.buf = self.buf[nbytes:]
+
+
 def _from_buffer(data):
     return np.frombuffer(data, np.uint64).reshape((-1, 2))
 
@@ -23,3 +50,31 @@ def sort_partials(args, encoded_stream, tdir, *, block_nbytes=1024*1024*1024):
             block.sort(axis=0)
             out.write(block.data)
     return partials
+
+
+def merge_stream(bufs, out, block_nbytes):
+    while bufs:
+        min_k = np.uint64(-1)
+        # For the first pass, we could, in principle, just seek() to the right
+        # position and read those 8 Bytes
+        for b in bufs:
+            ch = _from_buffer(b.peek(block_nbytes))
+            if not ch.size:
+                continue
+            cur = ch.T[0][-1]
+            if cur < min_k: min_k = cur
+        nbufs = []
+        cur = []
+        for b in bufs:
+            ch = _from_buffer(b.peek(block_nbytes))
+            if not ch.size:
+                continue
+            cur_c = np.searchsorted(ch.T[0], min_k, 'right')
+            b.consume(cur_c * 8 * 2)
+            cur.append(ch[:cur_c])
+            nbufs.append(b)
+        bufs = nbufs
+        if cur:
+            cur = np.concatenate(cur)
+            cur.sort(axis=0)
+            out.write(cur.data)
--- ksplit/tests/test_sort.py
+++ ksplit/tests/test_sort.py
@@ -10,14 +10,22 @@ TATATATATATTTCTTGTAATTTGTTGGAATACGAGAACATCGTCAATAATATATCGTATGAATTGAACCACACGGCACA
 HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
 '''
 
+def _encoded(fq):
+    out = BytesIO()
+    ksplit.encode_fastq({}, BytesIO(fq.encode('ascii')), out)
+    return out.getvalue()
+
+def _is_sorted(arr):
+    c = arr.copy()
+    c.sort()
+    return np.all(c == arr)
+
+
 def test_sort_partials(tmpdir):
     import operator
     import functools
 
-    out = BytesIO()
-    ksplit.encode_fastq({}, BytesIO(fq.encode('ascii')), out)
-    data = out.getvalue()
-
+    data = _encoded(fq)
     full = sort._from_buffer(data)
     sp = sort.sort_partials({}, BytesIO(data), tmpdir, block_nbytes=16*8*2)
     assert sum(stat(f).st_size for f in sp) == len(data)
@@ -26,6 +34,19 @@ def test_sort_partials(tmpdir):
     assert functools.reduce(operator.or_, (set(block.T[0]) for block in blocks)) == set(full.T[0])
 
     for b in blocks:
-        p = b.T[0].copy()
-        p.sort()
-        assert np.all(p == b.T[0])
+        assert _is_sorted(b.T[0])
+
+def test_merge(tmpdir):
+    out = BytesIO()
+    data = _encoded(fq)
+    full = sort._from_buffer(data)
+    sp = sort.sort_partials({}, BytesIO(data), tmpdir, block_nbytes=16*8*2)
+    out = BytesIO()
+    bufs = [sort.file_buffer(f) for f in sp]
+    sort.merge_stream(bufs, out, 128)
+    full_s = sort._from_buffer(out.getvalue())
+
+    assert _is_sorted(full_s.T[0])
+    assert set(full_s.T[0]) == set(full.T[0])
+    assert full_s.shape == full.shape
+
