c13deee78a8259aec5ebab260ab1eccce347c2d9
Luis Pedro Coelho <luis@luispedro.org>
2020-01-21 17:11:13 +0800

    ENH Return # of sequences from encode_fastq

--- ksplit/ksplit.py
+++ ksplit/ksplit.py
@@ -9,6 +9,13 @@ from .fastq import fastq_iter
 
 
 def encode_fastq(args, ifile, out):
+    '''
+    Returns the number of sequences encoded
+
+    Example
+    -------
+    encode_fastq( { .. }, open(ifname, 'rb'), open(ofname, 'wb'))
+    '''
     for i,seq in enumerate(fastq_iter(ifile)):
         seq = seq.replace(b'N', b'A')
         ks = kmers.kmers(seq)
@@ -17,4 +24,4 @@ def encode_fastq(args, ifile, out):
         ixs = np.repeat(np.array([i], dtype=np.uint64), len(ks))
         encoded = np.vstack([ks, ixs]).T.ravel()
         out.write(encoded.data)
-
+    return i + 1
