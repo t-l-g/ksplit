b5ea39566c39d0b236e5d26a9ebbe1987fb9358b
Luis Pedro Coelho <luis@luispedro.org>
2020-01-22 02:15:24 +1100

    ENH Support /1 and /2 at end of fastq header
    (Ignore them for purposes of interleave-parsing)

--- ksplit/fastq.py
+++ ksplit/fastq.py
@@ -2,6 +2,21 @@ from collections import namedtuple
 
 FastQSeq = namedtuple('FastQSeq', ['header', 'seq', 'qs'])
 
+def _compatible(h0, h1):
+    # This is a bit ugly, but works
+    if h0 == h1: return True
+    if type(h0) == str and \
+            h0.endswith('/1') and \
+            h1.endswith('/2') and \
+            h0[:-2] == h1[:-2]:
+                return True
+    if type(h0) == bytes and \
+            h0.endswith(b'/1') and \
+            h1.endswith(b'/2') and \
+            h0[:-2] == h1[:-2]:
+                return True
+    return False
+
 def fastq_iter1(ifile):
     '''
     for h, seq in fastq_iter1(open('example.fq', 'rb'))):
@@ -14,7 +29,7 @@ def fastq_iter1(ifile):
         qs = ifile.readline()
         if not qs:
             break
-        yield FastQSeq(h, seq.strip(), qs.strip())
+        yield FastQSeq(h.strip(), seq.strip(), qs.strip())
 
 
 def fastq_iter(base):
@@ -41,11 +56,11 @@ def fastq_iter(base):
     for seq in fastq_iter1(base):
         if prev is None:
             prev = seq
-        elif prev.header != seq.header:
-            yield (prev,)
-            prev = seq
-        else:
+        elif _compatible(prev.header, seq.header):
             yield (prev, seq)
             prev = None
+        else:
+            yield (prev,)
+            prev = seq
     if prev is not None:
         yield (prev,)
--- ksplit/tests/test_fastq.py
+++ ksplit/tests/test_fastq.py
@@ -1,4 +1,4 @@
-from ksplit.fastq import fastq_iter
+from ksplit.fastq import fastq_iter, _compatible
 from os import path
 
 BASEDIR = path.join(path.dirname(path.abspath(__file__)), 'data')
@@ -23,3 +23,9 @@ def test_interleaved_fastq():
         lens = [len(e)
                 for e in fastq_iter(ifile)]
     assert lens == [2,2,1,1]
+
+def test_compatible():
+    assert _compatible('@SRR4052021.7 7/1', '@SRR4052021.7 7/2')
+    assert not _compatible('@SRR4052021.7 7/1', '@SRR4052021.7 8/1')
+    assert _compatible('@SRR4052021.7', '@SRR4052021.7')
+    assert not _compatible('@SRR4052021.7 xz', '@SRR4052021.7 xy')
